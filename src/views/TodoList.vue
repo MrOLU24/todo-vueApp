083279ed2ec5a64254a9c16fa457a74348781cb7
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-18 22:25:51 +0100

    working on the todo list page

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,11 +1,47 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
+const todos = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+// Fetch To-Dos from API
+const fetchTodos = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (!response.ok) throw new Error("Failed to fetch To-Dos");
+    todos.value = await response.json();
+  } catch (err) {
+    error.value = err.message;
+  } finally {
+    loading.value = false;
+  }
+};
+
+
+onMounted(fetchTodos);
 </script>
 
 <template>
   <div class="p-6">
     <h1 class="text-2xl font-bold">To-Do List</h1>
-    <p>List of to-dos will go here...</p>
+
+    <p v-if="loading" class="text-blue-500">Loading...</p>
+    <p v-if="error" class="text-red-500">{{ error }}</p>
+
+    <ul v-if="!loading && !error" class="mt-4 space-y-3">
+      <li
+        v-for="todo in todos"
+        :key="todo.id"
+        class="p-3 bg-white shadow rounded-md border border-gray-200"
+      >
+        <router-link
+          :to="`/todo/${todo.id}`"
+          class="text-blue-600 hover:underline"
+        >
+          {{ todo.title }}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
