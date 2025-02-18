4f466f5c24372b224da34a2abf8f82797cd1d1bd
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-18 17:53:25 +0100

    arranging my todo list file

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
-<script setup>
+<template>
+  <div id="app">
+    <h1>Vue Todo App</h1>
+  </div>
+</template>
+
+<script>
+
 
 </script>
 
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
